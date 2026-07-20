a397255718c16b020643ed94174ace300c254ef6
kelleherkj <keith.kelleher@ncats.nih.gov>
2026-07-20 16:37:35 -0400

    add derived artifact for endogenous human metabolites from chebi
    generate sankey plots for TDL changes in Pharos

--- src/use_cases/working.py
+++ src/use_cases/working.py
@@ -1,9 +1,13 @@
-from src.use_cases.build_cli import run_arango_build_cli
+# from src.use_cases.build_cli import run_arango_build_cli
+#
+#
+# if __name__ == "__main__":
+#     run_arango_build_cli(
+#         build_name="working",
+#         database_name="metabolite_harmonization",
+#         primary_yaml="src/use_cases/working.yaml",
+#     )
+from src.core.data_registry import DataRegistry
 
-
-if __name__ == "__main__":
-    run_arango_build_cli(
-        build_name="working",
-        database_name="metabolite_harmonization",
-        primary_yaml="src/use_cases/working.yaml",
-    )
+registry = DataRegistry.from_registry_credentials('src/use_cases/secrets/aws_ifx_registry.yaml')
+registry.build_derived_artifact('chebi','endogenous_human_metabolites', dest='/private/tmp/ifx-registry-cache')
